978dc3cbe1bb5feab6ae7158ede1fb201b43a914
Ashraful Islam <19103189@iubat.edu>
2023-04-07 05:20:34 +0600

    done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -28,6 +28,7 @@ async function run() {
   try {
     const userCollection = client.db("ToDoDB").collection("users");
     const todoTitleCollection = client.db("ToDoDB").collection("title");
+    const taskCollection = client.db("ToDoDB").collection("task");
 
     //email is posted here
     app.post("/users", async (req, res) => {
@@ -36,19 +37,58 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+    //post todo title
     app.post("/api/todo/", async (req, res) => {
       const title = req.body;
       console.log(title);
       const result = await todoTitleCollection.insertOne(title);
       res.send(result);
     });
+    // get todo title
     app.get("/api/todo", async (req, res) => {
       const query = {};
-      console.log(query)
+      console.log(query);
       const result = await todoTitleCollection.find(query).toArray();
       res.send(result);
     });
 
+    // post task under title
+    app.put("/api/insertTaskInTodo/:id", async (req, res) => {
+      const id = req.params.id;
+      //   const payment = req.body;
+      const Tasks = req.body;
+      console.log(Tasks);
+
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      console.log(filter)
+      const updatedDoc = {
+        $set: {
+          task: Tasks,
+        },
+      };
+      const result = await todoTitleCollection.updateOne(filter, updatedDoc);
+      console.log(result);
+      res.send(result);
+    });
+
+    //  app.get("/api/TaskInTodo/:id", async (req, res) => {
+    //    const id = req.params.id;
+    //    const query = { _id: new ObjectId(id) };
+    //    const result = await todoTitleCollection.findOne(query);
+    //    res.send(result);
+    //  });
+
+
+    // delete todo title
+    app.delete("/api/deleteATodo/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      console.log(filter);
+      const result = await todoTitleCollection.deleteOne(filter);
+      res.send(result);
+    });
   } finally {
   }
 }
